00841105773352af769b4bf6a8daf48b5bc19968
yoodahun <dahun4032@gmail.com>
2023-09-30 13:19:41 +0900

    #9 page_factory.py #10 web_page_factory.py

---
+++ src/page_factory.py
@@ -0,0 +1,74 @@
+import re
+from typing import List
+
+from appium.webdriver import Remote, WebElement
+from selenium.common import TimeoutException
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+import selenium.webdriver.support.expected_conditions as EC
+
+
+class PageFactory:
+    def __init__(self, driver):
+        self.driver: Remote = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def _find_element(self, locator: tuple) -> WebElement:
+        return self.driver.find_element(*locator)
+
+    def _find_elements(self, locator: tuple) -> List[WebElement]:
+        return self.driver.find_elements(*locator)
+
+    def _find_element_for_wait(self, locator: tuple) -> WebElement:
+        return self.wait.until(EC.visibility_of_element_located(locator))
+
+    def _find_elements_for_wait(self, locator: tuple) -> List[WebElement]:
+        return self.wait.until(EC.visibility_of_all_elements_located(locator))
+
+    def _get_text(self, locator: tuple) -> str:
+        return self.__find_element_for_wait(locator).text
+
+    def _input(self, locator: tuple, text: str):
+        input_element = self.__find_element_for_wait(locator)
+        input_element.clear()
+        input_element.send_keys(text)
+
+    def _click(self, locator: tuple):
+        self.__find_element_for_wait(locator).click()
+
+    def _is_visible(self, locator: tuple) -> bool:
+        try:
+            return self.__find_element_for_wait(locator).is_displayed()
+        except TimeoutException:
+            return False
+
+    def _get_select_tag_element(self, locator: tuple) -> Select:
+        return Select(self.__find_element_for_wait(locator))
+
+    def _scroll_up_on_mobile(self, start_position: float = 0.7, end_position: float = 0.3):
+        size = self.driver.get_window_size()
+        start_y = size["height"] * start_position
+        end_y = size["height"] * end_position
+
+        start_x = size["width"] / 2
+
+        self.driver.swipe(start_x, start_y, start_x, end_y, 600)
+
+    def _scroll_down_on_mobile(self, start_position: float = 0.7, end_position: float = 0.3):
+        size = self.driver.get_window_size()
+        start_y = size["height"] * start_position
+        end_y = size["height"] * end_position
+
+        start_x = size["width"] / 2
+
+        self.driver.swipe(start_x, end_y, start_x, start_y, 600)
+
+    def _click_using_javascript_executor(self, locator: tuple):
+        element = self.__find_element_for_wait(locator)
+        self.driver.execute_script("arguments[0].click();", element)
+
+    def _change_window_taps(self, tap_index: int):
+        self.driver.switch_to.window(self.driver.window_handles[tap_index])
+
+    def _convert_string_to_int(self, text: str) -> int:
+        return int(re.sub(r"[^0-9]", "", text))
---
+++ src/web/pages/web_page_factory.py
@@ -0,0 +1,10 @@
+from src.page_factory import PageFactory
+
+
+class WebPageFactory(PageFactory):
+    
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def refresh_window(self):
+        self.driver.refresh()
