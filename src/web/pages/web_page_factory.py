8eed36bb97dda69266b0bad4d1d1e78ff29c3da6
yoodahun <dahun4032@gmail.com>
2023-10-01 14:11:36 +0900

    resolve #21, resolve #22

--- src/web/pages/web_page_factory.py
+++ src/web/pages/web_page_factory.py
@@ -1,12 +1,24 @@
 from src.page_factory import PageFactory
+from selenium.webdriver import ActionChains, Keys
 
 
 class WebPageFactory(PageFactory):
     
     def __init__(self, driver):
         super().__init__(driver)
+        self.driver = driver
+        self.action = ActionChains(self.driver)
 
     def refresh_window(self):
         self.driver.refresh()
+
+    def scroll_to_element(self, locator:tuple):
+        element = self._find_element_for_wait(locator)
+
+        self.action.move_to_element(element).perform()
+
+    def press_enter(self):
+        self.action.send_keys(Keys.RETURN).perform()
+
 
 
